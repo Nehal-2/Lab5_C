7d219dd27ab195d9ac23c39bff31655ba2eec89c
Nehal <NehalAlsuhaibani@gmail.com>
2024-10-30 10:44:55 +0300

    Second commit (Task 2 Correction)

--- task2/task2.c
+++ task2/task2.c
@@ -4,14 +4,14 @@ int main() {
         char chA = '1';
         int intA = 1;
         short shortA = 1;
-        double doubleA = 1;
+        long longA = 1;
 	char *p;
 
 	// Print the size to determine the architecture
         printf("Size of a character variable: %lu bytes.\n", sizeof(chA));
 	printf("Size of an integer variable: %lu bytes.\n", sizeof(intA));
 	printf("Size of a short variable: %lu bytes.\n", sizeof(shortA));
-	printf("Size of a double variable: %lu bytes.\n", sizeof(doubleA));	
+	printf("Size of a long variable: %lu bytes.\n", sizeof(longA));	
 	
 	// Print the address (to verify the size) and value of the first byte to check Endianness
 	p = &chA;
@@ -26,8 +26,8 @@ int main() {
         printf("Address of the first byte of the short is %p\n", p);
 	printf("The value of the variable is %d\n", *p);
 	
-	p = (char*)&doubleA;
-        printf("Address of the first byte of the double is %p\n", p);
+	p = (char*)&longA;
+        printf("Address of the first byte of the long is %p\n", p);
 	printf("The value of the variable is %d\n", *p);
 		
 	return 0;
